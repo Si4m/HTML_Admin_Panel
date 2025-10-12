0189254e02397d294662c914a8356bc5f6ff2eac
Irfan Uddin <mius952@gmail.com>
2025-10-12 13:09:31 +0600

    first commit

---
+++ function.php
@@ -0,0 +1,18 @@
+<?php
+
+function headers()
+{
+
+    include_once "include/header.php";
+}
+
+function sidebar()
+{
+    include_once "include/sidebar.php";
+}
+function footer()
+{
+    include_once "include/footer.php";
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,38 @@
+<!-- header-->
+<?php
+include_once "function.php";
+  headers();
+
+   sidebar();
+
+
+  ?>
+<!-- body-->
+
+                <div class="col-md-10 content">
+                    <div class="row">
+                        <div class="col-md-12 breadcumb_part">
+                            <div class="bread">
+                                <ul>
+                                    <li><a href=""><i class="fas fa-home"></i>Home</a></li>
+                                    <li><a href=""><i class="fas fa-angle-double-right"></i>Dashboard</a></li>                             
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 welcome_part">
+                            <p><span>Welcome Md.</span> Irfan Uddin</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+<!-- footer-->
+    <?php
+     footer();
+     ?>
